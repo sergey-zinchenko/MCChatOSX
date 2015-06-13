5f49bd7b6afccdc93068368c939b4f390df3ad6c
Sergey Zinchenko <sergey.zinchenko.rnd@gmail.com>
2015-06-13 18:38:30 +0300

    progress indicator

--- mchat/ViewController.m
+++ mchat/ViewController.m
@@ -14,11 +14,19 @@
 
 @implementation ViewController
 {
+    IBOutlet NSView *progressIndicatorHolderView;
+    IBOutlet NSProgressIndicator *progressIndicatorView;
+    IBOutlet NSTextField *progressIndicatorLabelView;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     [MCChatClient sharedInstance].deligate = self;
+    [progressIndicatorView startAnimation:self];
+    progressIndicatorHolderView.alphaValue = 0.8;
+    progressIndicatorHolderView.layer.backgroundColor = [[NSColor lightGrayColor] CGColor];
+    progressIndicatorHolderView.layer.cornerRadius = 10;
+    progressIndicatorHolderView.hidden = YES;
 }
 
 - (void)setRepresentedObject:(id)representedObject {
@@ -27,13 +35,13 @@
 
 - (void)onConnectAttemptStartedForClient:(MCChatClient *)client
 {
-    
+    progressIndicatorHolderView.hidden = NO;
 }
 
 - (void)onConnectAttemptEndedSuccessfully:(BOOL)successfully
                                 ForClient:(MCChatClient *)client
 {
-    
+    progressIndicatorHolderView.hidden = YES;
 }
 
 @end
