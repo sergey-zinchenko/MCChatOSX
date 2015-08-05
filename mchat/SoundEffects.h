84c698e4f0b58422d791c09d8c29ba0a74b7634e
Sergey Zinchenko <sergey.zinchenko.rnd@gmail.com>
2015-08-05 11:44:41 +0300

    SoundEffects idea

---
+++ mchat/SoundEffects.h
@@ -0,0 +1,21 @@
+//
+//  SoundEffects.h
+//  MChat
+//
+//  Created by Сергей Зинченко on 05.08.15.
+//  Copyright (c) 2015 Sergey Zinchenko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AVFoundation/AVFoundation.h>
+
+@interface SoundEffects : NSObject<AVAudioPlayerDelegate>
+
+- (void)playUserSound;
+- (void)playChatSound;
+- (void)playChatEventSound;
+- (void)playMessageSound;
+
++ (SoundEffects *)sharedInstance;
+
+@end
---
+++ mchat/SoundEffects.m
@@ -0,0 +1,82 @@
+//
+//  SoundEffects.m
+//  MChat
+//
+//  Created by Сергей Зинченко on 05.08.15.
+//  Copyright (c) 2015 Sergey Zinchenko. All rights reserved.
+//
+
+#import "SoundEffects.h"
+
+@interface SoundEffects ()
+-(void)playSound:(NSString *)sound;
+@end
+
+@implementation SoundEffects
+{
+    AVAudioPlayer *player;
+    NSDate *previousEnd;
+}
+
+- (void)audioPlayerDidFinishPlaying:(AVAudioPlayer *)p
+                       successfully:(BOOL)flag
+{
+    player = nil;
+    previousEnd = [NSDate date];
+}
+
+-(void)playSound:(NSString *)sound
+{
+    BOOL intervalOk = (!previousEnd)||fabs([previousEnd timeIntervalSinceNow]) > 1;
+    if (intervalOk&&!player) {
+        NSURL *fileURL = [[NSURL alloc] initFileURLWithPath:sound];
+        player = [[AVAudioPlayer alloc] initWithContentsOfURL:fileURL
+                                                        error:nil];
+        player.delegate = self;
+        [player play];
+    }
+}
+
+- (void)playUserSound
+{
+    NSString *soundFilePath =
+    [[NSBundle mainBundle] pathForResource:@"user-sound"
+                                    ofType:@"mp3"];
+    [self playSound:soundFilePath];
+}
+
+- (void)playChatSound
+{
+    NSString *soundFilePath =
+    [[NSBundle mainBundle] pathForResource:@"chat-sound"
+                                    ofType:@"mp3"];
+    [self playSound:soundFilePath];
+}
+
+- (void)playChatEventSound
+{
+    NSString *soundFilePath =
+    [[NSBundle mainBundle] pathForResource:@"chat-event-sound"
+                                    ofType:@"mp3"];
+    [self playSound:soundFilePath];
+}
+
+- (void)playMessageSound
+{
+    NSString *soundFilePath =
+    [[NSBundle mainBundle] pathForResource:@"message-sound"
+                                    ofType:@"mp3"];
+    [self playSound:soundFilePath];
+}
+
++ (SoundEffects *)sharedInstance
+{
+    static dispatch_once_t once;
+    static id sharedInstance;
+    dispatch_once(&once, ^{
+        sharedInstance = [[SoundEffects alloc] init];
+    });
+    return sharedInstance;
+}
+
+@end
